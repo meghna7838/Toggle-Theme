db5095fcf49433049a9f70fe2ce3c53d669c0ec4
meghna7838 <meghnatandon7838@gmail.com>
2024-05-26 20:18:13 +0530

    DONE

---
+++ q1_scaffold (1)/q1_scaffold/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./styles.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { theme: "light" };
+  }
+
+  handleClick = ()=>{
+    if(this.state.theme=="light")
+      {
+    this.setState({
+      theme : "dark"
+    })
+  }else{
+    this.setState({
+      theme : "light"
+    })
+  }
+  }
+
+  render() {
+    return (
+      <div className={`${this.state.theme}`}>
+      <div>
+        <button onClick={this.handleClick}>{this.state.theme==="light"?"Dark Theme":"Light Theme"}</button>
+        <h2>Why We Explore - NASA</h2>
+        <img
+          src="https://res.cloudinary.com/dl26pbek4/image/upload/v1671686299/launch-pad-67650_1920_p6izup.jpg"
+          alt="space shuttle"
+        />
+        <p>
+          Humans are driven to explore the unknown, discover new worlds, push
+          the boundaries of our scientific and technical limits, and then push
+          further. The intangible desire to explore and challenge the boundaries
+          of what we know and where we have been has provided benefits to our
+          society for centuries.
+        </p>
+      </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
